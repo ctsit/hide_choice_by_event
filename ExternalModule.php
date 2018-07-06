2ac08907a82387da728b16fab603970ffb46b60c
Dileep <rajputd@ufl.edu>
2018-07-06 11:52:31 -0400

    Add ExternalModule.php

---
+++ ExternalModule.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace HideChoiceByEvent\ExternalModule;
+
+use ExternalModules\AbstractExternalModule;
+use ExternalModules\ExternalModules;
+
+
+/**
+ * ExternalModule class for Project Ownership module.
+ */
+class ExternalModule extends AbstractExternalModule {
+
+}
+
+?>
