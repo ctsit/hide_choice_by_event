97dab145096727082b4c228f09a0f2a662d1668d
Dileep <rajputd@ufl.edu>
2018-07-06 12:02:49 -0400

    Add includeJs method to ExternalModule.php

--- ExternalModule.php
+++ ExternalModule.php
@@ -11,6 +11,16 @@ use ExternalModules\ExternalModules;
  */
 class ExternalModule extends AbstractExternalModule {
 
+    /**
+     * Includes a local JS file.
+     *
+     * @param string $path
+     *   The relative path to the js file.
+     */
+    protected function includeJs($path) {
+        echo '<script src="' . $this->getUrl($path) . '"></script>';
+    }
+
 }
 
 ?>
