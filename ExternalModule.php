92e7d7ef3d25a025e971713548df6aa6717183f0
Dileep <rajputd@ufl.edu>
2018-07-11 08:52:38 -0400

    Modify every_page_top_hook to generate a settings js variable when on the appropriate page

--- ExternalModule.php
+++ ExternalModule.php
@@ -17,7 +17,43 @@ class ExternalModule extends AbstractExternalModule {
     function hook_every_page_top($project_id) {
         if (PAGE == 'Design/online_designer.php' && $project_id) {
             $this->includeJs('js/modify_help_menu.js');
+            return;
         }
+
+        //if not on a data entry page or survey, then don't do anything
+        if (!in_array(PAGE, array('DataEntry/index.php', 'surveys/index.php'))) {
+            return;
+        }
+
+        global $Proj;
+        $settings = array();
+
+        //get the name of every field on this page.
+        $fields = array_keys($Proj->forms[$_GET['page']]['fields']);
+
+        foreach($fields as $field) {
+            $field_info = $Proj->metadata[$field];
+
+            //continue if the field does not have any action_tags at all.
+            if (!$action_tags = $field_info['misc']) {
+                continue;
+            }
+
+            //check if @HIDE-CHOICE-BY-EVENT is among action tags.
+            //did not use \Form::getValueInActionTag methods because of nested
+            //quotes inside JSON.
+            if (preg_match("/@HIDE-CHOICE-BY-EVENT\s*=\s*([\[].*[\]])/", $action_tags, $matches)) {
+
+                    //add to settings variable.
+                    $json_config = json_decode($matches[1], true);
+                    $settings[$field] = $json_config;
+            }
+
+        }
+
+        //make settings available for js scipts.
+        echo '<script>var HideChoiceByEvent = ' . json_encode($settings) . '</script>';
+
     }
 
     /**
